7047ed5839e85d18b80dbca04a0dc660716d888e
Zaki Ahmed <zaki.ahmed@worldvision.org.uk>
2021-02-05 07:56:18 +0000

    5229-RemoveUnitTestErrorCode

--- WV.FeatureSwitch.Dashboard.UnitTest/Mocks/ApiClientFactory/MockFeatureSwitchFactory.cs
+++ WV.FeatureSwitch.Dashboard.UnitTest/Mocks/ApiClientFactory/MockFeatureSwitchFactory.cs
@@ -1,16 +1,7 @@
 ﻿namespace WV.FeatureSwitch.Dashboard.UnitTest.Mocks.ApiClientFactory
 {
-    public class MockFeatureSwitchFactory : Mock<IMockFeatureSwitchFactory>
+    public class MockFeatureSwitchFactory
     {
-        public async Task<MockFeatureSwitchFactory> MockLoad(ChoosingPartyVM result)
-        {
-
-            IList<WorkflowStatusVM> statusVMs = new List<WorkflowStatusVM>();
-            Setup(x => x.GetWorkflowStatuses()).Returns(Task.Run(() => statusVMs));
-            Setup(x => x.Load(It.IsAny<int>()))
-                .Returns(Task.Run(() => result));
-            return await Task.FromResult(this);
-        }
-
+      
     }
 }
